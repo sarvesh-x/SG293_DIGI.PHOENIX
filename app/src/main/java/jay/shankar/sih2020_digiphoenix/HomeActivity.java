f4749d5c5278ed81c6c5d5ffe239d980c22ab5c9
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2020-08-01 11:31:57 +0530

    till login page

--- app/src/main/java/jay/shankar/sih2020_digiphoenix/HomeActivity.java
+++ app/src/main/java/jay/shankar/sih2020_digiphoenix/HomeActivity.java
@@ -1,6 +1,8 @@
 package jay.shankar.sih2020_digiphoenix;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
@@ -10,5 +12,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
