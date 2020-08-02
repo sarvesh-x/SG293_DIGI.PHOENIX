ae14eb1aad27691ec731a7799c75f198e00e36b1
Sarvesh Kumar <68198811+arreyshankar@users.noreply.github.com>
2020-08-02 09:42:00 +0530

    Add files via upload

--- app/src/main/java/jay/shankar/sih2020_digiphoenix/HomeActivity.java
+++ app/src/main/java/jay/shankar/sih2020_digiphoenix/HomeActivity.java
@@ -1,17 +1,118 @@
 package jay.shankar.sih2020_digiphoenix;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.ui.AppBarConfiguration;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private CardView show_tender_details, update_tender_details, budget_req, work_stats, contact_authority, current_progress, notifications, help_and_support;
+    public String ts_id;
+
+    public String exp_budget_status;
+    public String tender_id,tender_type,tender_name,tender_issue_date,tender_due_date,tender_budget,tender_details,tender_exp_budget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Toast.makeText(HomeActivity.this, ""+LoginActivity.user.get(2), Toast.LENGTH_SHORT).show();
+
+        Toolbar toolbar = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            toolbar = (Toolbar) findViewById(R.id.toolbar);
+        }
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                HomeActivity.this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        findViewById(R.id.floatingActionButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.openDrawer(GravityCompat.START);
+            }
+        });
+
+        TextView welcome_text = (TextView)findViewById(R.id.home_username);
+        welcome_text.setText(LoginActivity.user.get(0).toUpperCase().toString());
+        welcome_text.setVisibility(View.VISIBLE);
+
+        show_tender_details = (CardView) findViewById(R.id.home_tender_details);
+        update_tender_details = (CardView) findViewById(R.id.home_upload_todays_work);
+        budget_req = (CardView) findViewById(R.id.home_budget_requests);
+        work_stats = (CardView) findViewById(R.id.home_overall_statistics);
+        contact_authority = (CardView) findViewById(R.id.home_contact_authority);
+        current_progress = (CardView) findViewById(R.id.home_current_progress);
+        notifications = (CardView) findViewById(R.id.home_notifications);
+        help_and_support = (CardView) findViewById(R.id.home_help_and_support);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(HomeActivity.this);
+
+        show_tender_details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,TenderDetailsActivity.class));
+            }
+        });
+
+        update_tender_details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(HomeActivity.this,UpdateTodaysWorkActivity.class));
+
+            }
+        });
+        budget_req.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        work_stats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        contact_authority.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        notifications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        help_and_support.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
@@ -22,5 +123,38 @@ public class HomeActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_account) {
+
+            Toast.makeText(HomeActivity.this, "Account Settings Here", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_settings) {
+
+            Toast.makeText(HomeActivity.this, "Settings Here", Toast.LENGTH_SHORT).show();
+
+        } else if (id == R.id.nav_about) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(HomeActivity.this);
+            alert.setTitle("About");
+           // alert.setMessage(R.string.dialog_message);
+            alert.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            alert.create();
+            alert.show();
+        } else if (id == R.id.nav_exit) {
+            finish();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- app/src/main/java/jay/shankar/sih2020_digiphoenix/ThirdPartySurveyActivity.java
+++ app/src/main/java/jay/shankar/sih2020_digiphoenix/ThirdPartySurveyActivity.java
@@ -1,10 +1,21 @@
 package jay.shankar.sih2020_digiphoenix;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
 
 public class ThirdPartySurveyActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,4 +24,5 @@ public class ThirdPartySurveyActivity extends AppCompatActivity {
 
 
     }
+
 }
