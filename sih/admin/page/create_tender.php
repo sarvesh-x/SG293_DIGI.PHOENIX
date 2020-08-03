37cdaf41a7b22c4569015efc6830d03556ac8b29
Sarvesh Kumar <68198811+arreyshankar@users.noreply.github.com>
2020-08-03 22:29:17 +0530

    Update create_tender.php

--- sih/admin/page/create_tender.php
+++ sih/admin/page/create_tender.php
@@ -260,10 +260,10 @@ function myFunctions(id) {
 						<div class="form-group">
 							<label>Range</label>
 				<select class="form-control" name="range" required id="fc">
-				<option value=".1">100</option>
-				<option value=".2">200</option>
-				<option value=".3">300</option>
-				<option value=".4">400</option>
+				<option value="100">100</option>
+				<option value="200">200</option>
+				<option value="300">300</option>
+				<option value="400">400</option>
 				</select>
 						</div>
 						<br>
@@ -336,4 +336,6 @@ function myFunctions(id) {
       </div>
     </div>
   </div>
+	
+
 	
